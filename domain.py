a67e2d6d5804f3e2347cfbb80c7985b638bd78b6
deriggi <john.deriggi@gmail.com>
2016-07-13 16:36:03 -0400

    adding sample usage of the industry class

--- domain.py
+++ domain.py
@@ -7,18 +7,35 @@ class Industry(object):
 		self.cam = []
 		self.cam_length = 10;
 
-	# assign 10 scores to random normal function	
+	# assign 10 scores from a random normal sample	
 	def assign_random_normal_scores(self):
 		for x in range (self.cam_length):
 			randy_normal = random()/2 - random()/2 + 0.5;
 			self.cam.append(randy_normal);
 
+	def output_cam(self):
+		for x in self.cam:
+			print(x)		
 
-# create a category
+
+######################################
+# example usage
+######################################
+
+# create an industry
 c1 = Industry("catering");
 
-# populate its cam with a random normal distribution
+# populate its cam 
 c1.assign_random_normal_scores();
 
+# print the list
+c1.output_cam()
 
-print(c1);
+# a list of names
+industry_names=  ["aviation", "concrete construction", "hydro engineering", "drilling", "oil extraction"]
+
+# create a bunch of industries from a list of names
+for namo in industry_names :
+	i = Industry(namo)
+	i.assign_random_normal_scores()
+
