d2e15692a5dc3695b423a38bd4f848514e0d1b8b
deriggi <john.deriggi@gmail.com>
2016-07-13 16:23:46 -0400

    industry is what holds a cam

--- domain.py
+++ domain.py
@@ -1,11 +1,13 @@
 from random import random
 
-class Category(object):
+class Industry(object):
 
-	def __init__(self):
+	def __init__(self, industry_name):
+		self.name = industry_name
 		self.cam = []
 		self.cam_length = 10;
 
+	# assign 10 scores to random normal function	
 	def assign_random_normal_scores(self):
 		for x in range (self.cam_length):
 			randy_normal = random()/2 - random()/2 + 0.5;
@@ -13,7 +15,7 @@ class Category(object):
 
 
 # create a category
-c1 = Category();
+c1 = Industry("catering");
 
 # populate its cam with a random normal distribution
 c1.assign_random_normal_scores();
