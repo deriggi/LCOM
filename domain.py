df172b3c85788f5089c17975357684e39ad79658
deriggi <john.deriggi@gmail.com>
2016-07-16 11:34:16 -0400

    initializing assessments scores

--- domain.py
+++ domain.py
@@ -1,4 +1,28 @@
-from random import random
+import random
+import math
+
+
+class Company(object):
+	MAX_COMPANY_SIZE = 10000;
+	ASSESSMENT_SCORE_SIZE = 25;
+
+	def __init__(self):
+		self.size = math.floor(random.random()*Company.MAX_COMPANY_SIZE)	
+		self.assessment_scores = []
+
+	# assigne random normal scores to the self assessment
+	def assign_assessment_scores(self):
+
+		# for each score
+		for x in range(Company.ASSESSMENT_SCORE_SIZE):
+
+			# get a random normal averaged at 3.5 with stdev of 1
+			randy_normal = random.normalvariate(3.5,1)
+
+			# scale by the size of the company
+			randy_normal = randy_normal*math.log(self.size)
+			self.assessment_scores.append(randy_normal);
+
 
 class Industry(object):
 
@@ -6,12 +30,14 @@ class Industry(object):
 		self.name = industry_name
 		self.cam = []
 		self.cam_length = 10;
-		self.cam_radius = 3.5;  # for the 0 to 7 score
+		self.cam_radius = 3.0;  # for the 0 to 7 score
+
 
 	# assign cam scores from a random normal sample	
 	def assign_random_normal_scores(self):
 		for x in range (self.cam_length):
-			randy_normal = random()*self.cam_radius - random()*self.cam_radius + self.cam_radius;
+			# (0 -> 7)
+			randy_normal = random.random()*self.cam_radius - random.random()*self.cam_radius + self.cam_radius;
 			self.cam.append(randy_normal);
 
 	def output_cam(self):
@@ -22,6 +48,17 @@ class Industry(object):
 ######################################
 # example usage
 ######################################
+
+
+
+# test random company size with 10 companies
+print("company sizes")
+for z in range(40):
+	c = Company()
+	c.assign_assessment_scores()
+	print(c.assessment_scores)
+
+
 
 # create an industry
 c1 = Industry("catering");
